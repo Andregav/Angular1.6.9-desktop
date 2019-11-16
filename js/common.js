4fa7748c6c7352fe1e829952c8f01cf02a7ccf8f
ANDRE <gav11016@mail.ru>
2019-11-16 02:07:30 +0200

    angular_test

--- js/common.js
+++ js/common.js
@@ -1,89 +1,128 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
-    $http.get("data/data.js")
-        .then(function(response) {
-            $scope.myWelcome = response.data;
+import { Data } from './../data/data.js';
+import { City } from './../data/city.js';
+import { Category } from './../data/category.js';
+
+var images = [
+    { image: "images/card-img1.png" },
+    { image: "images/card-img2.png" },
+    { image: "images/card-img3.png" },
+    { image: "images/card-img2.png" },
+    { image: "images/card-img3.png" },
+];
+
+Data.sort(function(a, b) {
+    return a.city - b.city;
+});
+
+for (let key in Data) {
+    for (let key1 in City) {
+        Data[key].city = City[key].name;
+    }
+}
+Data.sort(function(a, b) {
+    return a.category - b.category;
+});
+
+for (let key in Data) {
+    for (let key2 in Category) {
+        switch (Data[key].category) {
+            case 1:
+                Data[key].category = Category[0].name;
+                break;
+            case 2:
+                Data[key].category = Category[1].name;
+                break;
+            case 4:
+                Data[key].category = Category[key].name;
+                break;
+            case 5:
+                Data[key].category = Category[key].name;
+                break;
+        }
+    }
+}
+
+let dataPrice = Data.slice()
+
+dataPrice.sort(function(a, b) { return a.price - b.price });
+let minPrice = dataPrice[0].price;
+
+dataPrice.sort(function(a, b) { return b.price - a.price });
+let maxPrice = dataPrice[0].price;
+
+let on = { on: false };
+
+for (let key in Data) {
+    Object.assign(Data[key], on, images[key]);
+}
+
+var dataApp = angular.module('dataApp', []);
+
+dataApp.filter('unique', function() {
+    return function(collection, keyname) {
+        var output = [],
+            keys = [];
+
+        angular.forEach(collection, function(item) {
+            var key = item[keyname];
+            if (keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
         });
+        return output;
+
+
+
+    };
+});
+
+
+dataApp.filter('categoryFilter', function() {
+    return function(input, categories) {
+        if (!categories || categories.length === 0) return input;
+        var out = [];
+        angular.forEach(input, function(item) {
+            angular.forEach(categories, function(categ) {
+                if (item.category === categ.category) {
+                    out.push(item);
+                }
+            });
+        });
+        return out;
+    }
+});
+
+dataApp.filter('rangeFilter', function() {
+    return function(items, attr, min, max) {
+        var range = [],
+            min = parseFloat(min),
+            max = parseFloat(max);
+        for (var i = 0, l = items.length; i < l; ++i) {
+            var item = items[i];
+            if (item[attr] <= max && item[attr] >= min) {
+                range.push(item);
+            }
+        }
+        return range;
+    };
 });
 
 
 
-// ---- custom dropdown----
-var x, i, j, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
-x = document.getElementsByClassName("custom-select");
-for (i = 0; i < x.length; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    /*for each element, create a new DIV that will act as the selected item:*/
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /*for each element, create a new DIV that will contain the option list:*/
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 0; j < selElmnt.length; j++) {
-        /*for each option in the original select element,
-        create a new DIV that will act as an option item:*/
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function(e) {
-            /*when an item is clicked, update the original select box,
-            and the selected item:*/
-            var y, i, k, s, h;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < s.length; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("same-as-selected");
-                    for (k = 0; k < y.length; k++) {
-                        y[k].removeAttribute("class");
-                    }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
-                }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
-        /*when the select box is clicked, close any other select boxes,
-        and open/close the current select box:*/
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-    });
-}
+dataApp.controller('dataCtrl', function($scope, $http) {
+    $scope.data = Data;
+    $scope.minPrice = minPrice;
+    $scope.maxPrice = maxPrice;
 
-function closeAllSelect(elmnt) {
-    /*a function that will close all select boxes in the document,
-    except the current select box:*/
-    var x, y, i, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    for (i = 0; i < y.length; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
-        } else {
-            y[i].classList.remove("select-arrow-active");
-        }
-    }
-    for (i = 0; i < x.length; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-        }
-    }
-}
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);
+    $scope.ui = {
+        min: $scope.minPrice,
+        max: $scope.maxPrice,
+        sort: 'name'
+    };
+});
 
-// ---end custom dropdown---
+
 
 // ---range-slider---
 
